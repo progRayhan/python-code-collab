98151fd8541ebc23d754f8668708679afdb2c222
Rayhan <prog.rayhan1@gmail.com>
2022-10-16 22:22:24 +0600

    print function added

---
+++ print.py
@@ -0,0 +1,18 @@
+# function = who can done some work
+# print function = print function can print something(string, number/digit)
+# print() =====> this is print function
+
+# string example
+# "Hello World"
+# 'Tonne'
+# "677"
+# double or single coutation doesn't matter
+
+# Number example
+# 123
+# 5
+# 6475409
+
+print("Hi Tonne")
+print(456)
+print("I am very very hungry")
